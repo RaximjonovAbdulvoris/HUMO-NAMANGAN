0f99d4e71b8cac4d8c3e177fbb18c6294faf054b
RaximjonovAbdulvoris <119586651+RaximjonovAbdulvoris@users.noreply.github.com>
2026-09-16 16:45:13 +0500

    Update driver.py

--- bot/handlers/driver.py
+++ bot/handlers/driver.py
@@ -50,7 +50,7 @@ _pending_car_progress_tasks: dict[int, asyncio.Task] = {}
 ) = range(13)
 
 JOIN_GROUP = 13
-REQUIRED_GROUP = "@yangi_taxi_namangan"
+REQUIRED_GROUP = "@WB_HUMO_TAXI"
 JOIN_KEYBOARD = InlineKeyboardMarkup([
     [InlineKeyboardButton("Guruhga qo‘shilish", url="https://t.me/yangi_taxi_namangan")],
     [InlineKeyboardButton("✅ Tekshirish", callback_data="driver:check_membership")],
